c7d511330c313f422078c6190b7f4e41f9f8f764
chesha1 <chesha1@163.com>
2025-08-24 15:29:25 +0800

    chore: 升级OCR模型

--- configure.py
+++ configure.py
@@ -14,14 +14,12 @@ def configure_ocr_model():
         exit(1)
 
     ocr_dir = assets_dir / "resource" / "base" / "model" / "ocr"
-    if not ocr_dir.exists():  # copy default OCR model only if dir does not exist
+    if True:  # 总是使用远程的最新 OCR 模型
         shutil.copytree(
             assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
             ocr_dir,
             dirs_exist_ok=True,
         )
-    else:
-        print("Found existing OCR directory, skipping default OCR model import.")
 
 
 if __name__ == "__main__":
